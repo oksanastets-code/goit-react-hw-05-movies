36e2714c75a070c19b7302add600e60bf26fa0df
Oksana Stets <oksanastets73@gmail.com>
2022-01-24 23:31:47 +0200

    add styles of film card

--- src/components/FilmDataCard/FilmDataCard.jsx
+++ src/components/FilmDataCard/FilmDataCard.jsx
@@ -1,26 +1,43 @@
 import { Link } from 'react-router-dom';
-export default function FilmDataCard({ film }) { 
-    return (
-        <>
-          <img src={`https://image.tmdb.org/t/p/w500${film.poster_path}`} alt="poster" />
-          <h2>{film.original_title} ({ film.release_date})</h2>
-          <p>User Score: { film.vote_average}</p>        
-          <h3>Overview</h3>
-          <p>{film.overview}</p>
-          <h3>Genres</h3>
-            {/* <p>{film.genres}</p> */}
-            <hr />
-            <h4>Additional information</h4>
-            <ul>
-                <li>
-                    <Link to={`/movies/${film.id}/cast`}>Cast</Link>
-                </li>
-                <li>
-                    <Link to={`/movies/${film.id}/reviews`}>Reviews</Link>
-                </li>
-            </ul>
-            <hr />
-        </>
-    )
-
+import {
+  FilmCard,
+  Poster,
+  DetailsCard,
+  FilmTitle,
+  Datas,
+  DatasTitle,
+    AddTitle,
+    AddList,
+  AddItem
+} from './FilmDataCard.styled';
+export default function FilmDataCard({ film }) {
+  return (
+    <>
+      <FilmCard>
+        <Poster
+          src={`https://image.tmdb.org/t/p/w500${film.poster_path}`}
+          alt="poster"
+        />
+        <DetailsCard>
+          <FilmTitle>
+            {film.original_title} ({film.release_date})
+          </FilmTitle>
+          <Datas>User Score: {film.vote_average}</Datas>
+          <DatasTitle>Overview</DatasTitle>
+          <Datas>{film.overview}</Datas>
+          <DatasTitle>Genres</DatasTitle>
+          {/* <Datas>{film.genres}</Datas> */}
+        </DetailsCard>
+      </FilmCard>
+      <AddTitle>Additional information</AddTitle>
+      <AddList>
+        <AddItem>
+          <Link to={`/movies/${film.id}/cast`}>Cast</Link>
+        </AddItem>
+        <AddItem>
+          <Link to={`/movies/${film.id}/reviews`}>Reviews</Link>
+        </AddItem>
+      </AddList>
+    </>
+  );
 }
---
+++ src/components/FilmDataCard/FilmDataCard.styled.jsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+export const FilmCard=styled.div`
+    display: flex;
+    margin-bottom: 20px;
+`
+export const Poster = styled.img`
+border-radius: 5px;
+-webkit-box-shadow: 0px 0px 30px 0px rgba(34, 60, 80, 0.2);
+  -moz-box-shadow: 0px 0px 30px 0px rgba(34, 60, 80, 0.2);
+  box-shadow: 0px 0px 30px 0px rgba(34, 60, 80, 0.2);
+`;
+export const DetailsCard = styled.div`
+    margin-left: 18px;
+`;
+export const FilmTitle = styled.h2`
+    margin-top: 20px;
+    margin-bottom: 20px;
+    font-size: 30px;
+
+`;
+export const Datas = styled.p`
+     margin-bottom: 20px;
+`;
+export const DatasTitle = styled.h3`
+    margin-bottom: 20px;
+    font-size: 25px;
+`;
+export const AddTitle = styled.h4`
+     margin-bottom: 20px;
+    font-size: 22px;
+`;
+export const AddList = styled.ul`
+     margin-bottom: 12px;
+   list-style-position: inside;
+   
+`;
+export const AddItem = styled.li`
+     margin-bottom: 12px;
+    font-size:18px;
+      
+`;
